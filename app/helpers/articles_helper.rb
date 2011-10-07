bd11bf982ced80b6279898bd1f27334fb2e9c5e9
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-10-07 03:28:19 +0200

    Changed pictures on articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -40,6 +40,7 @@ module ArticlesHelper
 
   def article_image(picture_id, type, link)
     picture = Photo.find_by_id(picture_id)
+    if picture
     style = case type.to_s
       when "1" then "photo_full"
       when "2" then "photo_one_third_left"
@@ -55,6 +56,9 @@ module ArticlesHelper
       url = "<a href=/assets/#{picture.original_picture.url}><div class=#{style}><img src =/assets/#{pic_url}  /><br /><i>#{picture.credits}</i><br /><i>#{picture.image_text_en}</i></div></a>"
     else
       url = "<div class=#{style}><img src =/assets/#{pic_url}  /><br /><i>#{picture.credits}</i><br /><i>#{picture.image_text_en}</i></div>"
+    end
+    else
+    ""
     end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
   set_primary_key :id
+  
+  has_attached_file :spp_article_image, styles:  {
+    front_large: {geometry: "346x154#", processors: [:cropper] },
+    front_small: {geometry: "220x131#", processors: [:cropper_half] },
+    article: {geometry: "447x346#", processors: [:cropper_spp_one_third] }
+    }
 end
