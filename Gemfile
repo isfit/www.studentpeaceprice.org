98b18cc8e2bf9860dc004f1dd0cdd4932a5ede88
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-02-04 01:20:47 +0100

    Fjernet params-tab tull, og oppdatert til 3.2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.0'
+gem 'rails', '3.2.1'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'mysql2'
 gem "bluecloth"
@@ -9,8 +9,8 @@ gem 'paperclip'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', "  ~> 3.1.0"
-  gem 'coffee-rails', "~> 3.1.0"
+  gem 'sass-rails'
+  gem 'coffee-rails'
   gem 'uglifier'
 end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,17 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  
+
   helper :all # include all helpers, all the time
-  
+
   @side_quote = Quote.find(:all, :conditions=>"side = 1", :order=>:weight).last
   @top_quote = Quote.find(:all, :conditions=>"top = 1", :order=>:weight).last
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '279e060fc95634856c631f6e3d81d540'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :tab => params[:tab] }
+  end
 end
