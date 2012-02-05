31991130218824a175eb5caf480e94b6e55bac0f
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-02-05 11:11:19 +0100

    Trenger det bare ikke

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,18 +15,5 @@ class ArticlesController < ApplicationController
   # GET /articles/1.xml
   def show
     @article = Article.find(params[:id])
-    @picture_path ="#{Rails.root}/public/assets/spp_article_images/" + "#{@article.id}.jpg"
-    @picture = false
-    if File.exists?(@picture_path)
-      @picture_path = "/assets/spp_article_images/" + "#{@article.id}_3.jpg"
-      @picture = true
-    else
-      @picture_path= "/assets/default/spp_default.png"
-      @picture = true
-    end
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @article }
-    end
   end
 end
