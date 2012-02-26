34f6237d18c4dcac22ea8768ca7be942cc57ed6b
Dag-Inge Aas <daginge@gmail.com>
2012-02-26 19:10:41 +0100

    Changed more layout

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
   scope :sorted, order("weight DESC")
   
   has_attached_file :spp_article_image, styles:  {
-    front_large: {geometry: "346x154#", processors: [:cropper] },
+    front_large: {geometry: "620x365#", processors: [:cropper] },
     front_small: {geometry: "220x131#", processors: [:cropper_half] },
     article: {geometry: "447x346#", processors: [:cropper_spp_one_third] }
     }
