2d477e02210cc24d46fa3b8fdf63385761b83fd3
Christian Knappskog <ch_knapp@mac.com>
2012-09-19 21:51:47 +0200

    fixing new paperclip

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,10 +5,12 @@ class Article < ActiveRecord::Base
   scope :sorted, order("weight DESC")
   scope :norwegian, where("title_no IS NOT ''")
   scope :english, where("title_en IS NOT ''")
-  
-  has_attached_file :spp_article_image, styles:  {
-    front_large: {geometry: "620x365#", processors: [:cropper] },
-    front_small: {geometry: "220x131#", processors: [:cropper_half] },
-    article: {geometry: "447x346#", processors: [:cropper_spp_one_third] }
-    }
+
+  has_attached_file :spp_article_image, styles: {
+      front_large: {geometry: "620x365#", processors: [:cropper]},
+      front_small: {geometry: "220x131#", processors: [:cropper_half]},
+      article: {geometry: "447x346#", processors: [:cropper_spp_one_third]}
+  },
+                    url: "/system/spp_articles/:attachment/:id_partition/:style/:filename",
+                    path: ":rails_root/public/system/spp_articles/:attachment/:id_partition/:style/:filename"
 end
